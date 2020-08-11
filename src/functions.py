9c04719fceee79b3c7fd3926d7ebae413dd0481b
Krutarth <krutarthparag.74@hotmail.com>
2020-08-11 19:38:41 +0530

    improved code, made output colorful, added functionality to view songs

--- src/functions.py
+++ src/functions.py
@@ -2,33 +2,40 @@ import src.exceptions as exceptions
 from src.audio_downloader import AudioDownloader
 from src.json_handler import JsonHandler
 import src.storage as storage
+from src.m_logging import Logger
 
 import os
 
+logger = Logger()
 
 def download(params):    
     if storage.data["current_playlist"] == "":
-        print("Go to a playlist first")
+        logger.log("Go to a playlist first", color=logger.color.red, effect=logger.effect.bold_effect)
         return
 
     jh = JsonHandler()
     data = jh.readFile()    
 
     def clbck(id):        
-        for playlist_data in data["playlists"].values():
-            for song in playlist_data["songs"]:
-                if song["id"] == id:                    
-                    return False         
-        return True    
+        # for playlist_data in data["playlists"].values():
+        #     for song in playlist_data["songs"]:
+        #         if song["id"] == id:                    
+        #             return False         
+        # return True            
+        for f in os.listdir('./data'):
+            if f[:-4] == id:
+                return False
+        return True                
 
     def success_hook(response):
-        print("success")
+        logger.log(f"Successfully downloaded {params[1]} in {storage.data['current_playlist']}", color=logger.color.green, effect=logger.effect.bold_effect)
 
     def downloading_hook(downloaded_bytes, total_bytes, elapsed, eta, speed):
-        print(downloaded_bytes * 100/total_bytes)        
-    def null_hook(response):
-        pass
-    ad = AudioDownloader(downloading_hook, null_hook, success_hook)    
+        print(f"{logger.formattedText(downloaded_bytes *100/total_bytes, color=logger.color.yellow)}% completed")       
+        
+    def error_hook(response):
+        logger.log("An error occured during downloading", color=logger.color.red)
+    ad = AudioDownloader(downloading_hook, error_hook, success_hook)    
     data["playlists"][storage.data["current_playlist"]]["songs"].append({"id": ad.download_audio(params[0], clbck), "name": params[1]})
     jh.writeFile(data)
 
@@ -37,11 +44,11 @@ def make_playlist(params):
     data = jh.readFile()
     for playlist in data["playlists"].keys():
         if playlist == params[0]:
-            print("A Playlist by that name already exists")
+            logger.log("A Playlist by that name already exists", color=logger.color.red, effect=logger.effect.bold_effect)            
             return
     data["playlists"][params[0]] = {"songs": []}
     jh.writeFile(data)
-    print("Made Playlist Successfully")
+    logger.log("Made Playlist Successfully", color=logger.color.green)
     # storage.data["prompt"] = f"{params[0]}> "
     # storage.data["current_playlist"] = params[0]
     
@@ -49,24 +56,35 @@ def remove_playlist(params):
     jh = JsonHandler()
     data = jh.readFile()
     if params[0] not in data['playlists'].keys():
-        print('That playlist does not exist')
+        logger.log('That playlist does not exist', color=logger.color.red, effect=logger.effect.bold_effect)
         return
     del data['playlists'][params[0]]
     jh.writeFile(data)
-    print("Playlist removed successfully")     
+    logger.log("Playlist removed successfully", color=logger.color.yellow, effect=logger.effect.bold_effect)     
 
 def view_playlists():
     jh = JsonHandler()
-    data = jh.readFile() 
+    data = jh.readFile()     
     print('\n'.join(data['playlists'].keys()))
 
 def move_to_playlist(params):
     jh = JsonHandler()
     data = jh.readFile()
     if params[0] not in data['playlists'].keys():
-        print('That playlist does not exist')
+        logger.log('That playlist does not exist', color=logger.color.red, effect=logger.effect.bold_effect)
         return
-    storage.data["current_playlist"]=params[0]        
+    storage.data["current_playlist"]=params[0]   
+
+def view_songs():
+    jh = JsonHandler()
+    data = jh.readFile()
+    if storage.data["current_playlist"] == "":
+        logger.log("Go to a playlist first", color=logger.color.red, effect=logger.effect.bold_effect)
+        return
+
+    songs = [x["name"] for x in data['playlists'][storage.data["current_playlist"]]['songs']]
+
+    print('\n'.join(songs))           
 
 def nothing():
     pass
--- src/input_handler.py
+++ src/input_handler.py
@@ -1,6 +1,9 @@
 from src.exceptions import StopPlayerException
 from src.command import Command
 import src.functions as functions
+from src.m_logging import Logger
+
+logger = Logger()
 
 commands = [
     Command(["", "\t"], 0, functions.nothing),
@@ -9,7 +12,8 @@ commands = [
     Command(["mp", "make_playlist"], 1, functions.make_playlist),
     Command(["rp", "remove_playlist"], 1, functions.remove_playlist),    
     Command(["->", '=>', 'mt', 'move_to'], 1, functions.move_to_playlist),
-    Command(["pl", "playlists"], 0, functions.view_playlists)
+    Command(["pl", "playlists"], 0, functions.view_playlists),
+    Command(["s", "so"], 0, functions.view_songs)
 ]
 
 def getCommandData(inp: str)->dict:
@@ -21,14 +25,11 @@ def getCommandData(inp: str)->dict:
 def handle(inp):    
     gcd = getCommandData(inp)
     for command in commands:
-        if gcd["command"] in command.aliases and len(gcd["params"]) == command.params_no:
-            command.run(gcd["params"])
-            return 
-    print("Command with enough params not found")    
-
-    # if inp == "":
-    #     pass    
-    # elif inp == "quit":
-    #     raise StopPlayerException
-    # else:
-    #     print(inp)
+        if gcd["command"] in command.aliases:
+            if len(gcd["params"]) == command.params_no:
+                command.run(gcd["params"])
+                return 
+            else:
+                logger.log(f"Command takes {command.params_no} param{'' if command.params_no == 1 else 's'}", color=logger.color.red, effect=logger.effect.bold_effect)
+                return
+    logger.log("Command not found", color=logger.color.red, effect=logger.effect.bold_effect)    
